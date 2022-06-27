0b83b68b101beac283a118ad98567742ae910011
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-06-27 23:23:41 +0430

    Add Todo.vue template

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,13 +1,28 @@
 <template>
-  
+      <div class="col-12 mb-2">
+            <div class="d-flex justify-content-between align-items-center border rounded p-3">
+                  <div>
+                        <span> todo.text </span>
+                  </div>
+                  <div>
+                        <button type="button" class="btn btn-info btn-sm">edit</button>
+                        <button type="button" class="btn btn-danger btn-sm ml-1">delete</button>
+                  </div>
+            </div>
+
+            <div class="d-flex justify-content-between align-items-center border rounded p-3">
+                  <div>
+                        <input type="text" />
+                  </div>
+                  <div>
+                        <button type="button" class="btn btn-success btn-sm px-4">edit</button>
+                  </div>
+            </div>
+      </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
