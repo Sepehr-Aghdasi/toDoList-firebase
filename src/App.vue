767e057c0b8f737428a371104c268030e01cd341
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-06-27 22:23:12 +0430

    Add CSS reset

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
 <script></script>
 
 <style>
+* {
+      margin: 0;
+      padding: 0;
+      outline: none;
+      text-decoration: none;
+      list-style-type: none;
+      box-sizing: border-box;
+      text-transform: capitalize !important;
+}
 #app {
       font-family: Avenir, Helvetica, Arial, sans-serif;
       -webkit-font-smoothing: antialiased;
