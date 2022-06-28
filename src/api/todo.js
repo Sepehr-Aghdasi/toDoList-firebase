dcb88127099699735a9c85338249ceb69124c3cc
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-06-28 13:57:45 +0430

    Fix bug

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-      baseURL: "https://vue-todolist-183e4-default-rtdb.europe-west1.firebasedatabase.app/",
+      baseURL: "https://vue-todolist-183e4-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
 export default instance;
