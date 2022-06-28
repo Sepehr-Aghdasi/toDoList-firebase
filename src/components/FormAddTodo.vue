b9214c0fde58e18d8c4e9632ce25ff5091e73436
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-06-28 13:41:12 +0430

    Create AddTodo function to get todoText data

--- src/components/FormAddTodo.vue
+++ src/components/FormAddTodo.vue
@@ -4,21 +4,34 @@
                   <h1 class="h1 fw-bold">welcome!</h1>
                   <p class="fs-6 lead text-muted">to get started</p>
             </div>
-            <div class="col-auto ">
+            <div class="col-auto">
                   <form class="d-flex" @click.prevent action="#">
                         <input
+                              v-model="todoText"
                               type="text"
                               class="form-control mx-2"
                               placeholder="I want to do...."
                         />
-                        <button class="btn btn-primary mx-2">submit</button>
+                        <button @click="addTodo" class="btn btn-primary mx-2">submit</button>
                   </form>
             </div>
       </div>
 </template>
 
 <script>
-export default {};
+export default {
+      data() {
+            return {
+                  todoText: "",
+            };
+      },
+      methods: {
+            addTodo() {
+                  this.$emit("add-todo", this.todoText);
+                  this.todoText = "";
+            },
+      },
+};
 </script>
 
 <style></style>
