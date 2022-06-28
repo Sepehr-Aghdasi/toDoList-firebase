7e7bb3cd34379ed890503bacf4b3cddf9f096016
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-06-28 13:46:21 +0430

    Create todo.js file for API

---
+++ src/api/todo.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const instance = axios.create({
+      baseURL: "https://vue-todolist-183e4-default-rtdb.europe-west1.firebasedatabase.app/",
+});
+
+export default instance;
